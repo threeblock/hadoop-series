081702632117c967ddd7daa047c2e8f5aebf22b5
chenlei <threeblocks@163.com>
2018-08-29 22:54:54 +0800

    test modify

--- src/main/java/org/threeblocks/mapreduce/join/Join.java
+++ src/main/java/org/threeblocks/mapreduce/join/Join.java
@@ -3,7 +3,6 @@ package org.threeblocks.mapreduce.join;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import org.apache.commons.beanutils.BeanUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
